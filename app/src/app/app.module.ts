288327f68737de41727dacb176081425038056a8
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-10-02 03:05:50 +0200

    not found added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,46 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PreloaderComponent } from './components/layouts/preloader/preloader.component';
-import { HomeOneComponent } from './components/defaults/home-one/home-one.component';
-import { HomeTwoComponent } from './components/defaults/home-two/home-two.component';
-import { HomeThreeComponent } from './components/defaults/home-three/home-three.component';
-import { HomeFourComponent } from './components/defaults/home-four/home-four.component';
-import { HomeFiveComponent } from './components/defaults/home-five/home-five.component';
-import { HomeSixComponent } from './components/defaults/home-six/home-six.component';
-import { HomeSevenComponent } from './components/defaults/home-seven/home-seven.component';
-import { HomeEightComponent } from './components/defaults/home-eight/home-eight.component';
-import { HomeNineComponent } from './components/defaults/home-nine/home-nine.component';
-import { HomeTenComponent } from './components/defaults/home-ten/home-ten.component';
-import { HomeElevenComponent } from './components/defaults/home-eleven/home-eleven.component';
-import { HomeTwelveComponent } from './components/defaults/home-twelve/home-twelve.component';
-import { HomeThirteenComponent } from './components/defaults/home-thirteen/home-thirteen.component';
-import { HomeFourteenComponent } from './components/defaults/home-fourteen/home-fourteen.component';
-import { HomeFifteenComponent } from './components/defaults/home-fifteen/home-fifteen.component';
-import { HomeSixteenComponent } from './components/defaults/home-sixteen/home-sixteen.component';
-import { HomeSeventeenComponent } from './components/defaults/home-seventeen/home-seventeen.component';
-import { HomeEighteenComponent } from './components/defaults/home-eighteen/home-eighteen.component';
-import { HomeNineteenComponent } from './components/defaults/home-nineteen/home-nineteen.component';
-import { HomeTwentyComponent } from './components/defaults/home-twenty/home-twenty.component';
-import { HomeTwentyOneComponent } from './components/defaults/home-twenty-one/home-twenty-one.component';
-import { HomeTwentyTwoComponent } from './components/defaults/home-twenty-two/home-twenty-two.component';
-import { HomeTwentyThreeComponent } from './components/defaults/home-twenty-three/home-twenty-three.component';
-import { HomeTwentyFourComponent } from './components/defaults/home-twenty-four/home-twenty-four.component';
-import { HomeTwentyFiveComponent } from './components/defaults/home-twenty-five/home-twenty-five.component';
-import { HomeTwentySixComponent } from './components/defaults/home-twenty-six/home-twenty-six.component';
-import { HomeTwentySevenComponent } from './components/defaults/home-twenty-seven/home-twenty-seven.component';
-import { HomeTwentyEightComponent } from './components/defaults/home-twenty-eight/home-twenty-eight.component';
-import { HomeTwentyNineComponent } from './components/defaults/home-twenty-nine/home-twenty-nine.component';
-import { HomeThirtyComponent } from './components/defaults/home-thirty/home-thirty.component';
-import { HomeFortyComponent } from './components/defaults/home-forty/home-forty.component';
-import { HomeThirtyOneComponent } from './components/defaults/home-thirty-one/home-thirty-one.component';
-import { HomeThirtyTwoComponent } from './components/pages/home-thirty-two/home-thirty-two.component';
-import { HomeThirtyThreeComponent } from './components/defaults/home-thirty-three/home-thirty-three.component';
-import { HomeThirtyFourComponent } from './components/defaults/home-thirty-four/home-thirty-four.component';
-import { HomeThirtyFiveComponent } from './components/defaults/home-thirty-five/home-thirty-five.component';
-import { HomeThirtySixComponent } from './components/defaults/home-thirty-six/home-thirty-six.component';
-import { HomeThirtySevenComponent } from './components/defaults/home-thirty-seven/home-thirty-seven.component';
-import { HomeThirtyEightComponent } from './components/defaults/home-thirty-eight/home-thirty-eight.component';
-import { HomeThirtyNineComponent } from './components/defaults/home-thirty-nine/home-thirty-nine.component';
+import { Home } from './components/pages/home/home';
 import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
 import { WorksDetailsComponent } from './components/pages/works-details/works-details.component';
 import { ServicesDetailsComponent } from './components/pages/services-details/services-details.component';
@@ -53,56 +14,20 @@ import {IvyCarouselModule} from "angular-responsive-carousel";
 import {MatGridListModule} from "@angular/material/grid-list";
 import { FooterComponent } from './components/pages/footer/footer.component';
 import { SidenavComponent } from './components/pages/sidenav/sidenav.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PreloaderComponent,
-    HomeOneComponent,
-    HomeTwoComponent,
-    HomeThreeComponent,
-    HomeFourComponent,
-    HomeFiveComponent,
-    HomeSixComponent,
-    HomeSevenComponent,
-    HomeEightComponent,
-    HomeNineComponent,
-    HomeTenComponent,
-    HomeElevenComponent,
-    HomeTwelveComponent,
-    HomeThirteenComponent,
-    HomeFourteenComponent,
-    HomeFifteenComponent,
-    HomeSixteenComponent,
-    HomeSeventeenComponent,
-    HomeEighteenComponent,
-    HomeNineteenComponent,
-    HomeTwentyComponent,
-    HomeTwentyOneComponent,
-    HomeTwentyTwoComponent,
-    HomeTwentyThreeComponent,
-    HomeTwentyFourComponent,
-    HomeTwentyFiveComponent,
-    HomeTwentySixComponent,
-    HomeTwentySevenComponent,
-    HomeTwentyEightComponent,
-    HomeTwentyNineComponent,
-    HomeThirtyComponent,
-    HomeFortyComponent,
-    HomeThirtyOneComponent,
-    HomeThirtyTwoComponent,
-    HomeThirtyThreeComponent,
-    HomeThirtyFourComponent,
-    HomeThirtyFiveComponent,
-    HomeThirtySixComponent,
-    HomeThirtySevenComponent,
-    HomeThirtyEightComponent,
-    HomeThirtyNineComponent,
+    Home,
     BlogDetailsComponent,
     WorksDetailsComponent,
     ServicesDetailsComponent,
     FooterComponent,
-    SidenavComponent
+    SidenavComponent,
+    NotFoundComponent
   ],
     imports: [
         BrowserModule,
