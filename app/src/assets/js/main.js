d8a714b35197263090025808147d35b3d44a5784
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-10-06 01:57:24 +0200

    update

--- app/src/assets/js/main.js
+++ app/src/assets/js/main.js
@@ -97,55 +97,7 @@
 		$(".home-banner-content .default-btn").addClass("animated fadeInDown").css("opacity", "1");
 	});
 
-	// Typewriter JS
-	var TxtType = function(el, toRotate, period) {
-		this.toRotate = toRotate;
-		this.el = el;
-		this.loopNum = 0;
-		this.period = parseInt(period, 10) || 2000;
-		this.txt = '';
-		this.tick();
-		this.isDeleting = false;
-	};
-	TxtType.prototype.tick = function() {
-		var i = this.loopNum % this.toRotate.length;
-		var fullTxt = this.toRotate[i];
-		if (this.isDeleting) {
-			this.txt = fullTxt.substring(0, this.txt.length - 1);
-		} else {
-			this.txt = fullTxt.substring(0, this.txt.length + 1);
-		}
-		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-		var that = this;
-		var delta = 200 - Math.random() * 100;
-		if (this.isDeleting) { delta /= 2; }
-		if (!this.isDeleting && this.txt === fullTxt) {
-			delta = this.period;
-			this.isDeleting = true;
-		} else if (this.isDeleting && this.txt === '') {
-			this.isDeleting = false;
-			this.loopNum++;
-			delta = 500;
-		}
-		setTimeout(function() {
-			that.tick();
-		}, delta);
-	};
-	window.onload = function() {
-		var elements = document.getElementsByClassName('typewrite');
-		for (var i=0; i<elements.length; i++) {
-			var toRotate = elements[i].getAttribute('data-type');
-			var period = elements[i].getAttribute('data-period');
-			if (toRotate) {
-				new TxtType(elements[i], JSON.parse(toRotate), period);
-			}
-		}
-		// INJECT CSS
-		var css = document.createElement("style");
-		css.type = "text/css";
-		css.innerHTML = ".typewrite > .wrap { border-right: 0.07em solid #f4f4f4}";
-		document.body.appendChild(css);
-	};
+
 
 	// Tabs
 	// (function ($) {
@@ -267,26 +219,6 @@
 		}
 	});
 
-	// Count Time
-	function makeTimer() {
-		var endTime = new Date("September 20, 2020 17:00:00 PDT");
-		var endTime = (Date.parse(endTime)) / 1000;
-		var now = new Date();
-		var now = (Date.parse(now) / 1000);
-		var timeLeft = endTime - now;
-		var days = Math.floor(timeLeft / 86400);
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");
-	}
-	setInterval(function() { makeTimer(); }, 0);
 
 	// Particles JS
 	if(document.getElementById("particles-js")) particlesJS("particles-js", {
