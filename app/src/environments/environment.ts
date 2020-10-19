f4d5f8661d48c91a101df5274c4a2414ce3c516d
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-10-19 13:52:48 +0200

    preparing for production final steps

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: false,
     apiUrl: "https://admin.qendrimvllasa.com/docs",
-    graphqlEndpoint: "https://admin.qendrimvllasa.com/graphql",
+    graphqlEndpoint: "http://admin.qendrimvllasa.com/graphql",
 };
