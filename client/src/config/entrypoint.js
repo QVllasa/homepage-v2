214d8b0a596b9d23c71cb0fb180f9587badb060d
Qendrim Vllasa <qendrim.vllasa@gmail.com>
2020-11-05 23:11:18 +0100

    Version 1.0 released

--- client/src/config/entrypoint.js
+++
@@ -1,9 +0,0 @@
-export const ENTRYPOINT = process.env.REACT_APP_API_ENTRYPOINT;
-
-export const GRAPHQL_ENTRYPOINT = process.env.GRAPHQL_ENDPOINT;
-
-export const APP_ENTRYPOINT = process.env.APP_ENDPOINT;
-
-export const ADMIN_ENDPOINT = process.env.ADMIN_ENDPOINT;
-
-export const API_ENDPOINT = process.env.API_ENDPOINT;
